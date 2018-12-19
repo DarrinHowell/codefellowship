e99ea34ff82363593ef29c6f5eb29ab379caa459
Darrin Howell <dbhowell09@gmail.com>
2018-12-19 13:53:52 -0800

    app running after configuring webconfig file

--- src/main/java/com/darrinhowell/codefellowship/ApplicationUser.java
+++ src/main/java/com/darrinhowell/codefellowship/ApplicationUser.java
@@ -1,12 +1,16 @@
 package com.darrinhowell.codefellowship;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Collection;
 
 @Entity
-public class ApplicationUser {
+public class ApplicationUser implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,4 +35,39 @@ public class ApplicationUser {
     }
 
     public ApplicationUser () {}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
